fb03626dd5f97c3ee72d61c4b81f7f51a0411e4c
JP314 <jan.grau95@gmail.com>
2018-12-08 12:32:38 +0100

    Day 3 Part 2

---
+++ src/day3b.hs
@@ -0,0 +1,61 @@
+import Data.Char
+import Data.List.Split
+import qualified Data.Map.Strict as Map
+import Data.Maybe
+
+main = interact 
+    $ show
+    . filter (\(claim, count) -> size claim == count)
+    . Map.assocs
+    . Map.fromListWith (+)
+    . filter ((==1) . snd)
+    . Map.elems
+    . insertPositions Map.empty 
+    . concatMap getPositions 
+    . map parseClaim 
+    . lines
+
+data Claim = Claim {
+    indet :: Int,
+    x :: Int,
+    y :: Int,
+    w :: Int,
+    h :: Int
+} deriving (Show, Ord, Eq)
+
+defaultClaim = Claim { indet = 0, x = 0, y = 0, w = 0, h = 0 }
+
+size :: Claim -> Int
+size c = w c * h c
+
+type Position = ((Int, Int), Claim)
+type Fabric = Map.Map (Int,Int) (Claim, Int)
+
+insertClaim :: Fabric -> Claim -> Fabric
+insertClaim fabric claim = insertPositions fabric $ getPositions claim
+
+getPositions :: Claim -> [Position]
+getPositions claim = [ ((x,y), claim) 
+        | x <- [ x claim .. x claim + w claim - 1 ]
+        , y <- [ y claim .. y claim + h claim - 1 ]
+    ]
+
+insertPositions :: Fabric -> [Position] -> Fabric
+insertPositions fabric = foldl insertPosition fabric
+
+insertPosition :: Fabric -> Position -> Fabric
+insertPosition fabric (coord, claim) = (Map.alter $ Just . mapSnd (+1) . fromMaybe (claim, 0)) coord fabric
+
+parseClaim :: String -> Claim
+parseClaim xs = case parseNumbers xs of
+    [ ident, x, y, w, h ]  -> Claim ident x y w h
+    _                      -> error $ "Invalid claim: " ++ show xs
+
+parseNumbers :: String -> [Int]
+parseNumbers = map read . split (dropDelims . dropBlanks . condense $ whenElt $ not . isDigit)
+
+mapFst :: (a -> c) -> (a, b) -> (c, b)
+mapFst f (x, y) = (f x, y)
+
+mapSnd :: (b -> c) -> (a, b) -> (a, c)
+mapSnd f (x, y) = (x, f y)
