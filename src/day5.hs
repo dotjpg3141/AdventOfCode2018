698dfe6fe4c77143a0d0acbf6ad47203a84a4ba3
JP314 <jan.grau95@gmail.com>
2018-12-09 13:10:47 +0100

    Day 5: Optimizing react, Part 2

--- src/day5.hs
+++ src/day5.hs
@@ -2,6 +2,7 @@ import AOC
 import Data.Char
 import Data.Maybe
 import Control.Applicative
+import qualified Data.Set as Set
 
 main :: IO ()
 main = solving 5 strat1 strat2 parseInput
@@ -13,19 +14,22 @@ strat1 :: String -> Int
 strat1 = length . react
 
 react :: String -> String
-react xs = fromJust
-    . last
-    . takeWhile isJust
-    $ iterate (>>= reactSingle) (Just xs)
+react = reverse . flip react' []
 
-reactSingle :: String -> Maybe String
-reactSingle (a:b:xs) = if canReact a b
-    then reactSingle xs <|> Just xs
-    else (a:) <$> ( reactSingle $ b:xs )
-reactSingle xs = Nothing
+react' :: String -> String -> String
+react' []     ys = ys
+react' (x:xs) [] = (react' xs [x])
+react' (x:xs) (y:ys) = if canReact x y
+    then react' xs ys
+    else react' xs (x:y:ys)
 
 canReact :: Char -> Char -> Bool
 canReact a b = toUpper a == toUpper b && isUpper a /= isUpper b
 
-strat2 :: String -> String
-strat2 = const ""
+strat2 :: String -> Int
+strat2 xs = minimum
+          . map (\c -> length . react $ filter ( (/= c) . toLower ) xs )
+          . Set.toList
+          . Set.fromList
+          . map toLower
+          $ xs
